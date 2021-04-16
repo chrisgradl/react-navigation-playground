a6d39669e1b9fa9909b752d41fe7e7e9ce2b1bc1
cgradl <christian.gradl@icloud.com>
2021-04-16 06:57:33 +0200

    added Redux Persist

--- App.web.tsx
+++ App.web.tsx
@@ -3,18 +3,22 @@ import "react-native-gesture-handler";
 import React from "react";
 import Playground from "./components/Playground";
 import { Provider } from "react-redux";
-import store from "./redux/store";
-
+import StoreConfig from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import { View } from "react-native";
 import PaperWrapper from "./components/PaperWrapper";
+
+const { persistor, store } = StoreConfig();
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <Provider store={store}>
-        <PaperWrapper>
-          <Playground />
-        </PaperWrapper>
+        <PersistGate persistor={persistor}>
+          <PaperWrapper>
+            <Playground />
+          </PaperWrapper>
+        </PersistGate>
       </Provider>
     </View>
   );
--- redux/store.ts
+++ redux/store.ts
@@ -1,17 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import navigators from "./NavigatorReducer";
 import rootId from "./RootIdReducer";
 import inspector from "./SelectedInspectorReducer";
 import theme from "./ThemeReducer";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  navigators,
+  rootId,
+  inspector,
+  theme,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
-  reducer: {
-    navigators,
-    rootId,
-    inspector,
-    theme,
-  },
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== "production",
 });
 
@@ -23,4 +33,7 @@ export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export default store;
+export default () => {
+  const persistor = persistStore(store);
+  return { store, persistor };
+};
