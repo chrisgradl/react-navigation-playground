462b50ab545856006218c096b39b3918d9df6c9c
cgradl <christian.gradl@icloud.com>
2021-08-25 10:37:57 +0200

    updated to next.js 11 and expo 42

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
 // @generated: @expo/next-adapter@2.1.77
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#shared-steps
 
-module.exports = { presets: ['@expo/next-adapter/babel'], plugins: ['@babel/plugin-proposal-class-properties']};
+module.exports = {
+  presets: ["@expo/next-adapter/babel"],
+  overrides: [
+    {
+      test: "./node_modules/react-native-reanimated/*",
+      plugins: ["@babel/plugin-proposal-class-properties"],
+    },
+    {
+      test: "./node_modules/@expo/vector-icons/*",
+      plugins: ["@babel/plugin-proposal-class-properties"],
+    },
+  ],
+};
--- next.config.js
+++ next.config.js
@@ -1,28 +1,28 @@
-// @generated: @expo/next-adapter@2.1.77
-// Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
-
 const { withExpo } = require("@expo/next-adapter");
+const withPlugins = require("next-compose-plugins");
+const withTM = require("next-transpile-modules")(["react-native-web"]);
 const withFonts = require("next-fonts");
 const withImages = require("next-images");
 
-module.exports = withExpo(
-  withFonts(
-    withImages({
-      projectRoot: __dirname,
-      async headers() {
-        return [
+const nextConfig = {
+  projectRoot: __dirname,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: [
           {
-            // Apply these headers to all routes in your application.
-            source: "/(.*)",
-            headers: [
-              {
-                key: "Content-Security-Policy",
-                value: "connect-src 'self';",
-              },
-            ],
+            key: "Content-Security-Policy",
+            value: "connect-src 'self';",
           },
-        ];
+        ],
       },
-    })
-  )
+    ];
+  },
+};
+
+module.exports = withPlugins(
+  [withTM, [withExpo, { projectRoot: __dirname }], withFonts],
+  nextConfig
 );
