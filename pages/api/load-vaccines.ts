b6e7f33f98672fbe62654201a8c9eceb23d328fd
cgradl <christian.gradl@icloud.com>
2022-06-12 12:09:44 +0200

    added cors header

--- pages/api/load-vaccines.ts
+++ pages/api/load-vaccines.ts
@@ -1,15 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import fetch from 'isomorphic-unfetch'
+import fetch from "isomorphic-unfetch";
 
 export default async function projects(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    try {
-        const vaccRes = await fetch('https://gitlab.com/elga-gmbh/termgit/-/raw/5c16a016d9acda29ac1502e36d445652c42c63d9/terminologies/ValueSet-eimpf-impfstoffe/ValueSet-eimpf-impfstoffe.4.fhir.json');
-        const data = await vaccRes.json();
-        return res.status(200).json(data);
-    } catch (e) {
-        return res.status(500).json({ error: e });
-    }
+  try {
+    const vaccRes = await fetch(
+      "https://gitlab.com/elga-gmbh/termgit/-/raw/5c16a016d9acda29ac1502e36d445652c42c63d9/terminologies/ValueSet-eimpf-impfstoffe/ValueSet-eimpf-impfstoffe.4.fhir.json"
+    );
+    const data = await vaccRes.json();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    return res
+      .status(200)
+      .json(data);
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
 }
